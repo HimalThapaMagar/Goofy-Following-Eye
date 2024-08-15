9f1c41bc72eb743c6a3091d82d60a79831e77a16
HimalThapaMagar <himalthapamagar@proton.me>
2024-08-15 19:34:39 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# goofy_eyes
+
+A new Flutter project.
+
+Trying to recreate the goofy eye that follows you in frontend.
---
+++ android/app/src/main/kotlin/com/example/goofy_eyes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.goofy_eyes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
